b11c1ce4d5e49464b3c31482ec0258fa8ac55ed5
benuh <blaisonyl@gmail.com>
2025-03-25 10:47:10 -0500

    Add initial test cases for daily_max() and daily_min()

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,7 +7,11 @@ from inflammation.models import daily_mean
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
-    
+    """Calculate the daily mean of a 2D inflammation data array for each day.
+
+    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :returns: An array of mean values of measurements for each day.
+    """
 
     test_input = np.array([[0, 0],
                            [0, 0],
@@ -29,3 +33,62 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def daily_mean(data):
+   """Calculate the daily mean of a 2D inflammation data array for each day.
+
+   :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+   :returns: An array of mean values of measurements for each day.
+   """
+   return np.mean(data, axis=0)
+
+
+def daily_max(data):
+   """Calculate the daily maximum of a 2D inflammation data array for each day.
+
+   :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+   :returns: An array of max values of measurements for each day.
+   """
+   return np.max(data, axis=0)
+
+def daily_min(data):
+   """Calculate the daily minimum of a 2D inflammation data array for each day.
+
+   :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+   :returns: An array of minimum values of measurements for each day.
+   """
+   return np.min(data, axis=0)
+
+
+from inflammation.models import daily_max, daily_mean, daily_min
+...
+def test_daily_max():
+    """Test that max function works for an array of positive integers."""
+
+    test_input = np.array([[4, 2, 5],
+                           [1, 6, 2],
+                           [4, 1, 9]])
+    test_result = np.array([4, 6, 9])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min():
+    """Test that min function works for an array of positive and negative integers."""
+
+    test_input = np.array([[ 4, -2, 5],
+                           [ 1, -6, 2],
+                           [-4, -1, 9]])
+    test_result = np.array([-4, -6, 2])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+...
+
+
+import pytest
+from inflammation.models import daily_min
+...
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
